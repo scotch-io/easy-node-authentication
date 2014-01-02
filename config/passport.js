d3264d30a9b1ff9cd16bdaad1e0c1fb824587d29
Chris Sevilleja <chris@scotch.io>
2014-01-02 09:58:04 -0800

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -59,7 +59,7 @@ module.exports = function(passport) {
 
                 // set the user's local credentials
                 newUser.local.email    = email;
-                newUser.hashPassword(password); // use the generateHash function in our user model
+                newUser.local.password = newUser.generateHash(password); // use the generateHash function in our user model
 
 				// save the user
                 newUser.save(function(err) {
