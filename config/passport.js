c153c715ef34dfef869d56cabdf683d5d666b58c
Chris Sevilleja <chris@scotch.io>
2014-02-04 11:12:35 -0800

    what

--- config/passport.js
+++ config/passport.js
@@ -74,30 +74,6 @@ module.exports = function(passport) {
     },
     function(req, email, password, done) {
 
-<<<<<<< HEAD
-	// asynchronous
-        process.nextTick(function() {
-
-	        // find a user whose email is the same as the forms email
-	        // we are checking to see if the user trying to login already exists
-	        User.findOne({ 'local.email' :  email }, function(err, user) {
-	            // if there are any errors, return the error before anything else
-	            if (err)
-	                return done(err);
-	
-	            // if no user is found, return the message
-	            if (!user)
-	                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-	
-	            // if the user is found but the password is wrong
-	            if (!user.validPassword(password))
-	                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-	
-	            // all is well, return successful user
-	            return done(null, user);
-	        });
-	        
-=======
         // asynchronous
         process.nextTick(function() {
             // check if the user is already logged ina
@@ -139,7 +115,7 @@ module.exports = function(passport) {
                 });
 
             }
->>>>>>> linking
+
         });
 
     }));
