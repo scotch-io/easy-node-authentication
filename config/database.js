feca5d784960f726dcb6256bcb2cab16d792a01a
Leask Huang <i@leaskh.com>
2014-04-05 02:47:19 +0800

    updated templates

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@
 
 module.exports = {
 
-    'url' : 'mongodb://127.0.0.1:27017' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://117.121.25.247:27017' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
