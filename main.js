6414a9473f13e6bdcfd70db5c33aa0ffaad2f360
Leask Huang <i@leaskh.com>
2014-04-09 01:22:07 +0800

    upgrade main logic

--- main.js
+++ main.js
@@ -6,38 +6,54 @@
 // get all the tools we need
 var express  = require('express'),
     app      = express(),
-    config   = require('./config/server.js'),
-    port     = config.port,
     mongoose = require('mongoose'),
     passport = require('passport'),
     flash    = require('connect-flash'),
-    configDB = require('./config/database.js');
+    path     = require('path');
 
 // configuration ===============================================================
+var config   = require('./config/server.js'),
+    configDB = require('./config/database.js');
+if (!config || !configDB) {
+    console.log('Error loading configs!');
+    process.exit(1);
+}
+var port     = config.port;
 mongoose.connect(configDB.url); // connect to our database
-
 require('./config/passport')(passport); // pass passport for configuration
 
 app.configure(function() {
+    // settings
+    app.disable('x-powered-by');
+    app.set('strict routing', true);
+    app.use(express.favicon(__dirname + '/public/favicon.ico'));
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.urlencoded());
+    app.use(express.json());
 
-	// set up our express application
-	app.use(express.logger('dev')); // log every request to the console
-	app.use(express.cookieParser()); // read cookies (needed for auth)
-	app.use(express.bodyParser()); // get information from html forms
+    // set up our express application
+    app.use(express.logger('dev')); // log every request to the console
+    app.use(express.cookieParser()); // read cookies (needed for auth)
+    app.use(express.bodyParser()); // get information from html forms
 
-	app.set('view engine', 'ejs'); // set up ejs for templating
+    // set up ejs for templating
+    app.set('view engine', 'ejs');
 
 	// required for passport
 	app.use(express.session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 	app.use(passport.initialize());
 	app.use(passport.session()); // persistent login sessions
 	app.use(flash()); // use connect-flash for flash messages stored in session
-
 });
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+// 404
+app.all('*', function(req, res) {
+    res.redirect('/404');
+});
 
-// launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+// ON AIR! =====================================================================
+app.listen(port, function() {
+    console.log('<<<<<<< PriceBeater-Share is now ON AIR! >>>>>>>');
+});
