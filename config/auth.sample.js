305a01634a8cdf5c234501119df16785ad0603db
Leask Huang <i@leaskh.com>
2014-04-10 05:57:52 +0800

    upgraded config samples

--- config/auth.sample.js
+++ config/auth.sample.js
@@ -3,22 +3,41 @@
 // expose our config directly to our application using module.exports
 module.exports = {
 
-	'facebookAuth' : {
-		'clientID' 		: 'your-secret-clientID-here', // your App ID
-		'clientSecret' 	: 'your-client-secret-here', // your App Secret
-		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
-	},
+    'facebookAuth' : {
+        'clientID' 		 : 'your-secret-clientID-here', // your App ID
+		'clientSecret' 	 : 'your-client-secret-here', // your App Secret
+		'callbackURL'    : 'http://localhost:8030/auth/facebook/callback'
+        'expire'         : 60 * 60 * 24 * 60
+    },
 
-	'twitterAuth' : {
-		'consumerKey' 		: 'your-consumer-key-here',
-		'consumerSecret' 	: 'your-client-secret-here',
-		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
-	},
+    'twitterAuth' : {
+        'consumerKey' 	 : 'your-consumer-key-here',
+		'consumerSecret' : 'your-client-secret-here',
+		'callbackURL' 	 : 'http://localhost:8030/auth/twitter/callback'
+    },
 
-	'googleAuth' : {
-		'clientID' 		: 'your-secret-clientID-here',
-		'clientSecret' 	: 'your-client-secret-here',
-		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
-	}
+    'googleAuth' : {
+        'clientID' 		 : 'your-secret-clientID-here',
+		'clientSecret' 	 : 'your-client-secret-here',
+		'callbackURL' 	 : 'http://localhost:8030/auth/google/callback'
+    },
+
+    'renrenAuth' : {
+        'clientID'       : 'your-secret-clientID-here', // your App ID
+        'clientSecret'   : 'your-client-secret-here', // your App Secret
+        'callbackURL'    : 'http://localhost:8030/auth/renren/callback'
+    },
+
+    'weiboAuth' : {
+        'clientID'       : 'your-secret-clientID-here',
+        'clientSecret'   : 'your-client-secret-here',
+        'callbackURL'    : 'http://localhost:8030/auth/weibo/callback'
+        'expire'         : 60 * 60 * 24 * 1
+    },
+
+    'awsAuth' : {
+        'bucket'         : 'type-bucket-here',
+        'acl'            : 'public-read'
+    }
 
 };
