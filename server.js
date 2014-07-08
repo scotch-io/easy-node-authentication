af2630963898fbd7f6496018d041e5b60223794b
Chris Sevilleja <chris@scotch.io>
2014-07-08 07:55:41 -0700

    updating bodyParser

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ require('./config/passport')(passport); // pass passport for configuration
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true });
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
