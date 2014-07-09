a34b7d7a65b6c8b3361f5e368ef88632029232b2
Chris Sevilleja <chris@scotch.io>
2014-07-09 07:31:34 -0700

    fixing syntax errors #11

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ require('./config/passport')(passport); // pass passport for configuration
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
-app.use(bodyParser.urlencoded({ extended: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
