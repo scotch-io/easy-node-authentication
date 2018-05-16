a4d55a73da35bd4578da4740aebf07c6c1cce10a
bsears90 <bsears@vampeo.com>
2018-05-15 23:01:59 -0400

    add static files

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.configure(function() {
 	app.use(passport.initialize());
 	app.use(passport.session()); // persistent login sessions
 	app.use(flash()); // use connect-flash for flash messages stored in session
+    app.use(express.static('public'))
 
 });
 
